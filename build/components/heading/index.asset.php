48251b670b291ea88005e664d9a4077307342c6f
mac <mac@MacBook-Pro-3.local>
2025-04-04 13:46:06 +0300

    heading tag edit

--- build/components/heading/index.asset.php
+++ build/components/heading/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '2e4fe0fad6760f52644c');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c95dd0ff921716ae7c2e');
